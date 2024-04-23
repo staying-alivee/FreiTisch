d6af2c83e435ecccf81a387c8ae56176eb86aae9
Vika Doan <tkimchidoan499@gmail.com>
2024-04-23 21:05:30 +0200

    + form and colors added

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,12 @@
 <template>
     <h1>Login</h1>
+     <form>
+         <div>
+         <input type="text" id="username" name="username" placeholder="Username" />
+         <input type="text" id="password" name="password" placeholder="Password" />
+         </div>
+     </form>
+     <button>LOGIN</button>
 </template>
 
 <script>
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,5 +1,16 @@
 <template>
     <h1>Register</h1>
+    <form>
+       <div>
+       <input type="text" id="givenname" name="givenname" placeholder="Given name" />
+       <input type="text" id="surname" name="surname" placeholder="Surname" />
+       <input type="email" id="email" name="email" placeholder="Email" />
+       <input type="text" id="username" name="username" placeholder="Username" />
+       <input type="text" id="password" name="password" placeholder="Password" />
+       <input type="text" id="password2" name="password2" placeholder="Repeat password" />
+      </div>
+     </form>
+     <button>REGISTER</button>
 </template>
 
 <script>
